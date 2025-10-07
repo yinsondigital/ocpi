c507b133b88c30d7eee239c51b849740c337ffb6
Shawn Agmo <shawn@agmostudio.com>
2025-10-07 10:28:38 +0800

    hotfix: version endpoint trailing slash

--- py_ocpi/modules/versions/api/main.py
+++ py_ocpi/modules/versions/api/main.py
@@ -13,3 +13,12 @@ async def get_versions(versions=Depends(get_versions_)):
         data=versions,
         **status.OCPI_1000_GENERIC_SUCESS_CODE,
     )
+
+
+@router.get("/versions/", response_model=OCPIResponse)
+async def get_versions(versions=Depends(get_versions_)):
+    return OCPIResponse(
+        data=versions,
+        **status.OCPI_1000_GENERIC_SUCESS_CODE,
+    )
+
